e36af6911465546daa2b84cf1f868f0a26175351
Brando-23 <usainbol147@gmail.com>
2025-01-24 11:30:58 +0530

    second

--- movies/src/components/Header.jsx
+++ movies/src/components/Header.jsx
@@ -18,6 +18,8 @@ export const Header = () => {
             <li className="nav-item"><NavLink to="/movies/top" className="nav-link">Top Movies</NavLink></li>
             <li className="nav-item"><NavLink to="/movies/upcoming" className="nav-link">Add Movies</NavLink></li>
           </ul>
+
+          {/* inga filter varanuminga filter varanum */}
           <form action="#">
             <input type="search" className="form-control " placeholder="search"/>
           </form>
